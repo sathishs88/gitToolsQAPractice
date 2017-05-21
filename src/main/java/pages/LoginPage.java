9b369b8be4b3f1c4c9dcd8abf102776d0e38e063
sathishs88 <sathish.swaminathan24@gmail.com>
2017-05-21 15:46:11 +0530

    update new params from gitbash

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -29,8 +29,8 @@ public class LoginPage extends BaseFunctions{
 	}
 	
 	public ProfilePage LoginToApp(){
-		txt_UserName.sendKeys("test2");
-		txt_Password.sendKeys("test2");
+		txt_UserName.sendKeys("test4");
+		txt_Password.sendKeys("test4");
 		btn_Login.click();
 		return new ProfilePage(driver);
 	}
